3650fbc2ef1d5ccaef766086fc7d76792d0eeb49
guofeng.my <guofeng.my@bytedance.com>
2022-10-29 04:26:47 +0800

    unrolling decode merge writer

--- lucene/core/src/java/org/apache/lucene/util/packed/DirectReader.java
+++ lucene/core/src/java/org/apache/lucene/util/packed/DirectReader.java
@@ -108,6 +108,7 @@ public class DirectReader {
     return new LongValues() {
 
       private final long[] buffer = new long[MERGE_BUFFER_SIZE];
+      private final long tail = numValues - MERGE_BUFFER_SIZE;
       private long blockIndex = -1;
 
       @Override
@@ -127,60 +128,16 @@ public class DirectReader {
 
       private void fillBuffer(long index) throws IOException {
         // NOTE: we're not allowed to read more than 3 bytes past the last value
-        if (index >= numValues - MERGE_BUFFER_SIZE) {
+        if (index >= tail) {
           // 128 values left or less
           final LongValues slowInstance = getInstance(slice, bitsPerValue, baseOffset);
           final int numValuesLastBlock = Math.toIntExact(numValues - index);
           for (int i = 0; i < numValuesLastBlock; ++i) {
             buffer[i] = slowInstance.get(index + i);
           }
-        } else if ((bitsPerValue & 0x07) == 0) {
-          // bitsPerValue is a multiple of 8: 8, 16, 24, 32, 30, 48, 56, 64
-          final int bytesPerValue = bitsPerValue / Byte.SIZE;
-          final long mask = bitsPerValue == 64 ? ~0L : (1L << bitsPerValue) - 1;
-          long offset = baseOffset + (index * bitsPerValue) / 8;
-          for (int i = 0; i < MERGE_BUFFER_SIZE; ++i) {
-            if (bitsPerValue > Integer.SIZE) {
-              buffer[i] = slice.readLong(offset) & mask;
-            } else if (bitsPerValue > Short.SIZE) {
-              buffer[i] = slice.readInt(offset) & mask;
-            } else if (bitsPerValue > Byte.SIZE) {
-              buffer[i] = Short.toUnsignedLong(slice.readShort(offset));
-            } else {
-              buffer[i] = Byte.toUnsignedLong(slice.readByte(offset));
-            }
-            offset += bytesPerValue;
-          }
-        } else if (bitsPerValue < 8) {
-          // bitsPerValue is 1, 2 or 4
-          final int valuesPerLong = Long.SIZE / bitsPerValue;
-          final long mask = (1L << bitsPerValue) - 1;
-          long offset = baseOffset + (index * bitsPerValue) / 8;
-          int i = 0;
-          for (int l = 0; l < 2 * bitsPerValue; ++l) {
-            final long bits = slice.readLong(offset);
-            for (int j = 0; j < valuesPerLong; ++j) {
-              buffer[i++] = (bits >>> (j * bitsPerValue)) & mask;
-            }
-            offset += Long.BYTES;
-          }
         } else {
-          // bitsPerValue is 12, 20 or 28
-          // Read values 2 by 2
-          final int numBytesFor2Values = bitsPerValue * 2 / Byte.SIZE;
-          final long mask = (1L << bitsPerValue) - 1;
-          long offset = baseOffset + (index * bitsPerValue) / 8;
-          for (int i = 0; i < MERGE_BUFFER_SIZE; i += 2) {
-            final long l;
-            if (numBytesFor2Values > Integer.BYTES) {
-              l = slice.readLong(offset);
-            } else {
-              l = slice.readInt(offset);
-            }
-            buffer[i] = l & mask;
-            buffer[i + 1] = (l >>> bitsPerValue) & mask;
-            offset += numBytesFor2Values;
-          }
+          long offset = baseOffset + ((index * bitsPerValue) >>> 3);
+          UnrollingDecoder.decode(slice, offset, buffer, bitsPerValue);
         }
       }
     };
