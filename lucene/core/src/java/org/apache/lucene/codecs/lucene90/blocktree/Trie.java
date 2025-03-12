e25a32f0d5b10bc95d5a29c204b4b5b052626e4d
guofeng.my <guofeng.my@bytedance.com>
2025-03-12 13:28:59 +0800

    add lifecyce management

--- lucene/core/src/java/org/apache/lucene/codecs/lucene90/blocktree/Trie.java
+++ lucene/core/src/java/org/apache/lucene/codecs/lucene90/blocktree/Trie.java
@@ -39,6 +39,12 @@ class Trie {
 
   record Output(long fp, boolean hasTerms, BytesRef floorData) {}
 
+  enum Status {
+    UNSAVED,
+    SAVED,
+    DESTROYED
+  }
+
   private static class Node {
     final int label;
     final LinkedList<Node> children;
@@ -52,6 +58,7 @@ class Trie {
     }
   }
 
+  Status status = Status.UNSAVED;
   final Node root = new Node(0, null, new LinkedList<>());
 
   Trie(BytesRef k, Output v) {
@@ -70,6 +77,10 @@ class Trie {
   }
 
   void putAll(Trie trie) {
+    if (status != Status.UNSAVED || trie.status != Status.UNSAVED) {
+      throw new IllegalStateException("tries should be unsaved");
+    }
+    trie.status = Status.DESTROYED;
     putAll(this.root, trie.root);
   }
 
@@ -120,6 +131,10 @@ class Trie {
   }
 
   void save(DataOutput meta, IndexOutput index) throws IOException {
+    if (status != Status.UNSAVED) {
+      throw new IllegalStateException("only unsaved trie can be saved");
+    }
+    status = Status.SAVED;
     meta.writeVLong(index.getFilePointer());
     saveNodes(index);
     meta.writeVLong(root.fp);
