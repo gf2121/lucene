f81ccb5c64d495e610527e5721f9ff7d6b4e5d3f
guofeng.my <guofeng.my@bytedance.com>
2025-03-14 18:59:52 +0800

    benchmark step 128

--- lucene/core/src/java/org/apache/lucene/util/bkd/DocIdsWriter.java
+++ lucene/core/src/java/org/apache/lucene/util/bkd/DocIdsWriter.java
@@ -39,7 +39,7 @@ public final class DocIdsWriter {
   // These signs are legacy, should no longer be used in the writing side.
   private static final byte LEGACY_DELTA_VINT = (byte) 0;
 
-  private static final int[] BATCHES = new int[] {512, 128};
+  private static final int[] BATCHES = new int[] {128};
 
   private final int[] scratch;
 
@@ -324,11 +324,11 @@ public final class DocIdsWriter {
   private static void readDelta16(IndexInput in, int count, int[] docIds) throws IOException {
     final int min = in.readVInt();
     int k = 0;
-    for (int bound = count - 511; k < bound; k += 512) {
-      in.readInts(docIds, k, 256);
-      // Can be inlined to make offsets consistent so that loop get auto-vectorized.
-      inner16(k, docIds, 256, min);
-    }
+//    for (int bound = count - 511; k < bound; k += 512) {
+//      in.readInts(docIds, k, 256);
+//      // Can be inlined to make offsets consistent so that loop get auto-vectorized.
+//      inner16(k, docIds, 256, min);
+//    }
     for (int bound = count - 127; k < bound; k += 128) {
       in.readInts(docIds, k, 64);
       inner16(k, docIds, 64, min);
@@ -348,12 +348,12 @@ public final class DocIdsWriter {
 
   private void readInts24(IndexInput in, int count, int[] docIDs) throws IOException {
     int k = 0;
-    for (int bound = count - 511; k < bound; k += 512) {
-      in.readInts(scratch, k, 384);
-      shift(k, docIDs, scratch, 384);
-      // Can be inlined to make offsets consistent so that loop get auto-vectorized.
-      remainder24(k, docIDs, scratch, 128, 256, 384);
-    }
+//    for (int bound = count - 511; k < bound; k += 512) {
+//      in.readInts(scratch, k, 384);
+//      shift(k, docIDs, scratch, 384);
+//      // Can be inlined to make offsets consistent so that loop get auto-vectorized.
+//      remainder24(k, docIDs, scratch, 128, 256, 384);
+//    }
     for (int bound = count - 127; k < bound; k += 128) {
       in.readInts(scratch, k, 96);
       shift(k, docIDs, scratch, 96);
