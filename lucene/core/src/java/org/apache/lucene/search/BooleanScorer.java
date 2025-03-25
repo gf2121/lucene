4b1f4e964c7eb173356b9b5c379994b94b706924
guofeng.my <guofeng.my@bytedance.com>
2025-03-25 20:09:03 +0800

    iter

--- lucene/core/src/java/org/apache/lucene/search/BooleanScorer.java
+++ lucene/core/src/java/org/apache/lucene/search/BooleanScorer.java
@@ -191,7 +191,7 @@ final class BooleanScorer extends BulkScorer {
           // This doesn't apply live docs, so we'll need to apply them later
           it.intoBitSet(max, matching, base);
           upTo = nextUnsetBit(upTo - base) + base;
-          System.out.println(upTo);
+          System.out.println(upTo - base);
         }
       } else {
         for (; doc < max; doc = it.nextDoc()) {
