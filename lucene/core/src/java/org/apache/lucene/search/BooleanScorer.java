e03516ef7311c9e517727bf68802f288736c6e3b
guofeng.my <guofeng.my@bytedance.com>
2025-03-25 20:14:02 +0800

    disable vector mask

--- lucene/core/src/java/org/apache/lucene/search/BooleanScorer.java
+++ lucene/core/src/java/org/apache/lucene/search/BooleanScorer.java
@@ -191,7 +191,6 @@ final class BooleanScorer extends BulkScorer {
           // This doesn't apply live docs, so we'll need to apply them later
           it.intoBitSet(max, matching, base);
           upTo = nextUnsetBit(upTo - base) + base;
-          System.out.println(upTo - base);
         }
       } else {
         for (; doc < max; doc = it.nextDoc()) {
--- lucene/core/src/java21/org/apache/lucene/internal/vectorization/PanamaVectorUtilSupport.java
+++ lucene/core/src/java21/org/apache/lucene/internal/vectorization/PanamaVectorUtilSupport.java
@@ -769,7 +769,7 @@ final class PanamaVectorUtilSupport implements VectorUtilSupport {
 
   // Experiments suggest that we need at least 8 lanes so that the overhead of going with the vector
   // approach and counting trues on vector masks pays off.
-  private static final boolean ENABLE_FIND_NEXT_GEQ_VECTOR_OPTO = INT_SPECIES.length() >= 8;
+  private static final boolean ENABLE_FIND_NEXT_GEQ_VECTOR_OPTO = INT_SPECIES.length() >= 16;
 
   @Override
   public int findNextGEQ(int[] buffer, int target, int from, int to) {
