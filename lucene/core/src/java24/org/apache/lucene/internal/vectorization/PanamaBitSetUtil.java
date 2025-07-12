1ff822e9f61f96274506050faff83bb5df8429b0
guofeng.my <guofeng.my@bytedance.com>
2025-07-12 23:56:36 +0800

    iter

--- lucene/core/src/java24/org/apache/lucene/internal/vectorization/PanamaBitSetUtil.java
+++ lucene/core/src/java24/org/apache/lucene/internal/vectorization/PanamaBitSetUtil.java
@@ -16,6 +16,8 @@
  */
 package org.apache.lucene.internal.vectorization;
 
+import java.util.stream.IntStream;
+import jdk.incubator.vector.IntVector;
 import jdk.incubator.vector.ByteVector;
 import jdk.incubator.vector.VectorMask;
 import jdk.incubator.vector.VectorOperators;
@@ -24,63 +26,43 @@ public class PanamaBitSetUtil extends BitSetUtil {
 
   static final PanamaBitSetUtil INSTANCE = new PanamaBitSetUtil();
 
-  private static final byte[] IDENTITY_BYTES = new byte[64];
-
-  static {
-    for (int i = 0; i < IDENTITY_BYTES.length; i++) {
-      IDENTITY_BYTES[i] = (byte) i;
-    }
-  }
+  private static final int[] IDENTITY = IntStream.range(0, Long.SIZE).toArray();
+  private static final int[] IDENTITY_MASK = IntStream.range(0, 16).map(i -> 1 << i).toArray();
+  private static final int MASK = (1 << IntVector.SPECIES_PREFERRED.length()) - 1;
 
   PanamaBitSetUtil() {}
 
   @Override
-  @SuppressWarnings("fallthrough")
   int word2Array(long word, int base, int[] docs, int offset) {
-    if (PanamaVectorConstants.PREFERRED_VECTOR_BITSIZE != 512) {
-      return super.word2Array(word, base, docs, offset);
+    int lWord = (int) word;
+    int hWord = (int) (word >>> 32);
+
+    IntVector identityMask = IntVector.fromArray(IntVector.SPECIES_PREFERRED, IDENTITY_MASK, 0);
+    for (int i = 0; i < Integer.SIZE; i += IntVector.SPECIES_PREFERRED.length()) {
+      VectorMask<Integer> mask = IntVector.broadcast(IntVector.SPECIES_PREFERRED, lWord)
+          .and(identityMask)
+          .compare(VectorOperators.NE, 0);
+      IntVector.fromArray(IntVector.SPECIES_PREFERRED, IDENTITY, i)
+          .add(base)
+          .compress(mask)
+          .reinterpretAsInts()
+          .intoArray(docs, offset);
+      offset += Integer.bitCount(lWord & MASK); // faster than mask.trueCount()
+      lWord >>>= IntVector.SPECIES_PREFERRED.length();
+    }
+    for (int i = 0; i < Integer.SIZE; i += IntVector.SPECIES_PREFERRED.length()) {
+      VectorMask<Integer> mask = IntVector.broadcast(IntVector.SPECIES_PREFERRED, hWord)
+          .and(identityMask)
+          .compare(VectorOperators.NE, 0);
+      IntVector.fromArray(IntVector.SPECIES_PREFERRED, IDENTITY, i)
+          .add(base)
+          .compress(mask)
+          .reinterpretAsInts()
+          .intoArray(docs, offset);
+      offset += Integer.bitCount(hWord & MASK); // faster than mask.trueCount()
+      hWord >>>= IntVector.SPECIES_PREFERRED.length();
     }
 
-    if (word == 0L) {
-      return offset;
-    }
-
-    int bitCount = Long.bitCount(word);
-
-    VectorMask<Byte> mask = VectorMask.fromLong(ByteVector.SPECIES_512, word);
-    ByteVector indices =
-        ByteVector.fromArray(ByteVector.SPECIES_512, IDENTITY_BYTES, 0).compress(mask);
-
-    switch ((bitCount - 1) >> 4) {
-      case 3:
-        indices
-            .convert(VectorOperators.B2I, 3)
-            .reinterpretAsInts()
-            .add(base)
-            .intoArray(docs, offset + 48);
-      case 2:
-        indices
-            .convert(VectorOperators.B2I, 2)
-            .reinterpretAsInts()
-            .add(base)
-            .intoArray(docs, offset + 32);
-      case 1:
-        indices
-            .convert(VectorOperators.B2I, 1)
-            .reinterpretAsInts()
-            .add(base)
-            .intoArray(docs, offset + 16);
-      case 0:
-        indices
-            .convert(VectorOperators.B2I, 0)
-            .reinterpretAsInts()
-            .add(base)
-            .intoArray(docs, offset);
-        break;
-      default:
-        throw new IllegalStateException(bitCount + "");
-    }
-
-    return offset + bitCount;
+    return offset;
   }
 }
